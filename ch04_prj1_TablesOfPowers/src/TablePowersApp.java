0dd3abec4573255eda38413f07401a5f01bdbd2e
unknown <motley.dylan60@gmail.com>
2021-06-05 18:06:04 -0400

    new adds

---
+++ ch04_prj1_TablesOfPowers/src/TablePowersApp.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class TablePowersApp {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Squares and Cubes table");
+		System.out.println();
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter an integer: ");
+		sc.next();
+		
+		
+		sc.close();
+		System.out.println("Bye");
+
+	}
+
+}
