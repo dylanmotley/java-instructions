36633d4cb527e34e5ae0f4bc288a82be44ae4cf8
unknown <motley.dylan60@gmail.com>
2021-06-07 10:43:57 -0400

    New Files

--- ch04_prj1_TablesOfPowers/src/TablePowersApp.java
+++ ch04_prj1_TablesOfPowers/src/TablePowersApp.java
@@ -7,21 +7,7 @@ public class TablePowersApp {
 		System.out.println();
 		
 		Scanner sc = new Scanner(System.in);
-		
-		System.out.print("Enter an integer: ");
-		sc.next();
-		
-		System.out.println("Number   Squared   Cubed");
-		System.out.println("======   =======   ======");
-		
-//		int i;
-// 		for (i = 0; i <= 100; i+=10) {
-//			System.out.println("Output: "+i);
-//		}
-		
-		
-		
-		
+
 		sc.close();
 		System.out.println("Bye");
 
