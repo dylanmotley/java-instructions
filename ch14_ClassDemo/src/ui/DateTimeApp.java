3ab9c870c12807f474ac9263f05784994833fdf9
unknown <motley.dylan60@gmail.com>
2021-06-18 11:43:00 -0400

    Compare Dates

--- ch14_ClassDemo/src/ui/DateTimeApp.java
+++ ch14_ClassDemo/src/ui/DateTimeApp.java
@@ -18,11 +18,31 @@ public class DateTimeApp {
 		System.out.println(halloween);
 		System.out.println(christmas);
 		
-		LocalDate july4th = LocalDate.parse("2021-7-4");
+		LocalDate july4th = LocalDate.parse("2021-07-04");
 		System.out.println("July 4th: "+july4th);
 		LocalDateTime laborDay = LocalDateTime.parse("2021-09-06T00:00");
 		System.out.println("Labor Day: "+laborDay);
 
+		System.out.println("getYear(): "+currentTimeStamp.getYear());
+		System.out.println("getMonth(): "+currentTimeStamp.getMonthValue());
+		System.out.println("getMonthValue(): "+currentTimeStamp.getMonthValue());
+		System.out.println("getDayOfMonth(): "+currentTimeStamp.getDayOfMonth());
+		System.out.println("getDayOfYear(): "+currentTimeStamp.getDayOfYear());
+		System.out.println("getDayOfWeek(): "+currentTimeStamp.getDayOfWeek());
+		System.out.println("getHour(): "+currentTimeStamp.getHour());
+		System.out.println("getMinute(): "+currentTimeStamp.getMinute());
+		System.out.println("getSecond(): "+currentTimeStamp.getSecond());
+		System.out.println("getNano(): "+currentTimeStamp.getNano());
+		
+		if (currentTimeStamp.isBefore(laborDay)) {
+		System.out.println("Today is before labor day");
+		}
+		if (christmas.isAfter(halloween)) {
+			System.out.println("Duh");
+		}
+		
+		System.out.println(halloween.compareTo(christmas));
+		System.out.println(christmas.compareTo(july4th));
 	}
 
 }
